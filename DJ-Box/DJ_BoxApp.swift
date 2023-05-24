59fd661c667bcca6a66e897f16750230aa498f61
林寧 <leelemons1999@gmail.com>
2023-05-24 10:02:38 +0800

    Initial Commit

---
+++ DJ-Box/DJ_BoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  DJ_BoxApp.swift
+//  DJ-Box
+//
+//  Created by 林寧 on 2023/5/24.
+//
+
+import SwiftUI
+
+@main
+struct DJ_BoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
