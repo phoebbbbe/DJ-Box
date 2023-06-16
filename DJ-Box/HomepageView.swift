2ee6d0a324fdc470f6557b3822c4233244b45794
phoebbbbe <leelemons1999@gmail.com>
2023-06-16 10:02:47 +0800

    modify HomepageView.swift for jump to other View by navigationlink

--- DJ-Box/HomepageView.swift
+++ DJ-Box/HomepageView.swift
@@ -11,48 +11,65 @@ struct HomepageView: View {
     @EnvironmentObject var songListManager: SongListManager
     
     var body: some View {
-        ZStack {
-            Color(red: 23/255, green: 22/255, blue: 46/255)
-               .ignoresSafeArea()
-            VStack {
-                Title(iconName: "music.note",title: "Create Playlist")
-                    .offset(x:0, y:-180)
-                ZStack {
-                    ForEach(songListManager.songLists) { list in
-                        SongListItem(listItem: list)
+        NavigationView {
+            ZStack {
+                Color(red: 23/255, green: 22/255, blue: 46/255)
+                   .ignoresSafeArea()
+                VStack {
+                    Title(iconName: "music.note",title: "Create Playlist")
+                        .offset(x:0, y:-180)
+                    ZStack {
+                        ForEach(songListManager.songLists) { list in
+                            NavigationLink {
+                                ListView()
+                                    .environmentObject(SongManager())
+                            } label: {
+                                SongListItem(listItem: list)
+                            }
                             .offset(x: CGFloat(list.offset_x), y: CGFloat(list.offset_y))
+
+                            
+                        }
                     }
+                    VStack {
+                        NavigationLink {
+                            FilterView()
+                                .environmentObject(SongManager())
+                        } label: {
+                            ZStack {
+                                Circle()
+                                    .fill(
+                                        LinearGradient(
+                                            gradient: Gradient(colors: [
+                                               Color(red: 0.66, green: 0.96, blue: 0.71),
+                                               Color(red: 0.80, green: 0.97, blue: 0.52),
+                                               Color(red: 0.84, green: 0.98, blue: 0.47),
+                                               Color(red: 0.66, green: 0.96, blue: 0.71)]),
+                                            startPoint: .leading,
+                                            endPoint: .trailing
+                                        )
+                                    )
+                                    .frame(width: 130, height: 130)
+                                    .shadow(color: .white, radius: 10, x: 0, y: 0)
+                                
+                                Image(systemName: "waveform")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 70, height: 70)
+                                    .foregroundColor(Color(red: 0.18, green: 0.17, blue: 0.35))
+                                    .shadow(color: .white, radius: 10, x: 0, y: 0)
+                                
+                            }
+                            .offset(x: 0, y: -50)
+                        }
+
+
+                    }
+                    
                 }
                 
-                // Search Button
-                ZStack {
-                    Circle()
-                        .fill(
-                            LinearGradient(
-                                gradient: Gradient(colors: [
-                                   Color(red: 0.66, green: 0.96, blue: 0.71),
-                                   Color(red: 0.80, green: 0.97, blue: 0.52),
-                                   Color(red: 0.84, green: 0.98, blue: 0.47),
-                                   Color(red: 0.66, green: 0.96, blue: 0.71)]),
-                                startPoint: .leading,
-                                endPoint: .trailing
-                            )
-                        )
-                        .frame(width: 130, height: 130)
-                        .shadow(color: .white, radius: 10, x: 0, y: 0)
-                    
-                    Image(systemName: "waveform")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70, height: 70)
-                        .foregroundColor(Color(red: 0.18, green: 0.17, blue: 0.35))
-                        .shadow(color: .white, radius: 10, x: 0, y: 0)
-                    
-                }
-                .offset(x: 0, y: -50)
+                
             }
-            
-            
         }
     }
 }
