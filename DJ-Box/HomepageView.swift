e87bd40943de1640c5c889ccc7c5f9a2b1a674c6
phoebbbbe <leelemons1999@gmail.com>
2023-06-18 18:14:31 +0800

    justfy HomepageView.swift

--- DJ-Box/HomepageView.swift
+++ DJ-Box/HomepageView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct HomepageView: View {
+    @EnvironmentObject var songManager: SongManager
     @EnvironmentObject var songListManager: SongListManager
     
     var body: some View {
@@ -17,55 +18,53 @@ struct HomepageView: View {
                    .ignoresSafeArea()
                 VStack {
                     Title(iconName: "music.note",title: "Create Playlist")
-                        .offset(x:0, y:-180)
+                        .offset(x:0, y:-230)
                     ZStack {
                         ForEach(songListManager.songLists) { list in
                             NavigationLink {
                                 ListView()
-                                    .environmentObject(SongManager())
+                                    .environmentObject(songManager)
                             } label: {
                                 SongListItem(listItem: list)
                             }
                             .offset(x: CGFloat(list.offset_x), y: CGFloat(list.offset_y))
 
-                            
+
                         }
-                    }
-                    VStack {
-                        NavigationLink {
-                            FilterView()
-                                .environmentObject(SongManager())
-                        } label: {
-                            ZStack {
-                                Circle()
-                                    .fill(
-                                        LinearGradient(
-                                            gradient: Gradient(colors: [
-                                               Color(red: 0.66, green: 0.96, blue: 0.71),
-                                               Color(red: 0.80, green: 0.97, blue: 0.52),
-                                               Color(red: 0.84, green: 0.98, blue: 0.47),
-                                               Color(red: 0.66, green: 0.96, blue: 0.71)]),
-                                            startPoint: .leading,
-                                            endPoint: .trailing
+                        VStack {
+                            NavigationLink {
+                                FilterView()
+                                    .environmentObject(songManager)
+                            } label: {
+                                ZStack {
+                                    Circle()
+                                        .fill(
+                                            LinearGradient(
+                                                gradient: Gradient(colors: [
+                                                   Color(red: 0.66, green: 0.96, blue: 0.71),
+                                                   Color(red: 0.80, green: 0.97, blue: 0.52),
+                                                   Color(red: 0.84, green: 0.98, blue: 0.47),
+                                                   Color(red: 0.66, green: 0.96, blue: 0.71)]),
+                                                startPoint: .leading,
+                                                endPoint: .trailing
+                                            )
                                         )
-                                    )
-                                    .frame(width: 130, height: 130)
-                                    .shadow(color: .white, radius: 10, x: 0, y: 0)
-                                
-                                Image(systemName: "waveform")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 70, height: 70)
-                                    .foregroundColor(Color(red: 0.18, green: 0.17, blue: 0.35))
-                                    .shadow(color: .white, radius: 10, x: 0, y: 0)
+                                        .frame(width: 130, height: 130)
+                                        .shadow(color: .white, radius: 10, x: 0, y: 0)
+                                    
+                                    Image(systemName: "waveform")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 70, height: 70)
+                                        .foregroundColor(Color(red: 0.18, green: 0.17, blue: 0.35))
+                                        .shadow(color: .white, radius: 10, x: 0, y: 0)
+                                    
+                                }
                                 
                             }
-                            .offset(x: 0, y: -50)
-                            
                         }
                     }
-                    
-                    
+
                 }
             }
             
@@ -76,6 +75,7 @@ struct HomepageView: View {
 struct HomepageView_Previews: PreviewProvider {
     static var previews: some View {
         HomepageView()
+            .environmentObject(SongManager())
             .environmentObject(SongListManager())
     }
 }
